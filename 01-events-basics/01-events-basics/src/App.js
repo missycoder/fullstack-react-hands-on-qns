6097b1dfa2d5ea655d40f8c492fb7941a324609a
missycoder mylan <noreen.taha@outlook.com>
2024-03-22 08:10:18 +0000

    updated 01

--- 01-events-basics/01-events-basics/src/App.js
+++ 01-events-basics/01-events-basics/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
-function App() {
-
-  const pop = () => {
-    alert("pop goes the weasel!")
-  }
-
-  return (
-    <React.Fragment>
-    <button onClick={}>Press me</button>
-    </React.Fragment>
-  );
-}
-
-export default App;
-
-
+export default function Popupreact() {
+	return (
+		<div>
+			<h4>Popup - SCTP-02 REACT</h4>
+			<Popup trigger=
+				{<button> Click here to open popup </button>}
+				position="right center">
+				<div>React Modules</div>
+        <ul>
+          <li>Introduction</li>
+          <li>Beginner</li>
+          <li>Intermediate</li>
+          <li>Advanced</li>
+        </ul>
+			</Popup>
+		</div>
+	)
+};
