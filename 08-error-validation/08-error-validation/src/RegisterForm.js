0201d55b99f9c732b1e95ee5ed7af38d070947fe
missycoder mylan <noreen.taha@outlook.com>
2024-03-28 05:43:23 +0000

    update registerForm.js

--- 08-error-validation/08-error-validation/src/RegisterForm.js
+++ 08-error-validation/08-error-validation/src/RegisterForm.js
@@ -1,28 +1,72 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function RegisterForm() {
 
-    const [userName, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [userName, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
-      <React.Fragment>
-        <div>
-            <label>Username:</label>
-            <input type="text" name="username"/>
-            <span class="error"></span>
-        </div>
-        <div>
-            <label>Email:</label>
-            <input type="text" name="email"/>
-            <span class="error"></span>
-        </div>
-        <div>
-            <label>Password:</label>
-            <input type="text" name="password"/>
-            <span class="error"></span>
-        </div>
-      </React.Fragment>
-    )
+  const [userErrorNotification, setUserErrorNotification] = useState("");
+  const [emailErrorNotification, setEmailErrorNotification] = useState("");
+  const [passwordErrorNotification, setPasswordErrorNotification] = useState("");
+
+  // regular expression
+  // meow123@gmail.com
+  // to prevent cross site scripting: should avoid some of these special characters
+  // avoid: %, *
+  const emailRegex = /^[a-zA-Z0-9._-] + @[a-zA-Z.-] + \.[A-Za-z.]{2,}$/
+  const textRegex = /^[a-zA-Z0-9] {0,15} $/
+
+
+  function validateAll() {
+    validateUserName();
+    validateEmail();
+    validatePassword();
+  }
+
+  function validateUserName() {
+    if (textRegex.test(userName)) {
+      setUserErrorNotification("");
+    } else {
+      setUserErrorNotification("Username must be less than 15 characters and cannot contain any special characters");
+    }
+  }
+
+  function validateEmail() {
+    if (emailRegex.test(email)) {
+      setEmailErrorNotification("");
+    } else {
+      setEmailErrorNotification("Invalid email format");
+    }
+  }
+
+  function validatePassword() {
+    if (textRegex.test(password)) {
+      setUserErrorNotification("");
+    } else {
+      setUserErrorNotification("Invalid password format");
+    }
+  }
+
+
+  return (
+    <React.Fragment>
+      <div>
+        <label>Username:</label>
+        <input type="text" name="username" onChange={(event) => setUsername(event.target.value)} />
+        <span class="error" style={{ color: "red" }}>{userErrorNotification}</span>
+      </div>
+      <div>
+        <label>Email:</label>
+        <input type="text" name="email" onChange={(event) => setEmail(event.target.value)} />
+        <span class="error" style={{ color: "red" }}>{emailErrorNotification}</span>
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="text" name="password" onChange={(event) => setPassword(event.target.value)} />
+        <span class="error" style={{ color: "red" }}>{passwordErrorNotification}</span>
+      </div>
+      <button onClick={() => validateAll()}>Submit</button>
+    </React.Fragment>
+  )
 }
